75b8d4323a9065943a99fffc5e6a3a0bd6909134
JohnMiz <napstar261@gmail.com>
2018-11-11 21:20:38 +0200

    Edited the UI, used ItemSelected and ItemTapped events.

--- MoviesAppTut/MoviesAppTut/View/MainPage.xaml.cs
+++ MoviesAppTut/MoviesAppTut/View/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +23,20 @@ namespace MoviesAppTut
 			   InitializeComponent();
 
 			   listView.ItemsSource = new List<MovieInfo> {
-					new MovieInfo{ ImageSource = "https://picsum.photos/458/354", Name ="Test", ReleaseYear="1999"},
-					new MovieInfo{ ImageSource = "https://picsum.photos/458/354", Name ="Test1", ReleaseYear="1994"},
-					new MovieInfo{ ImageSource = "https://picsum.photos/458/354", Name ="Test2", ReleaseYear="1993"},
+					new MovieInfo{ ImageSource = "https://picsum.photos/50/50", Name ="Test", ReleaseYear="1999"},
+					new MovieInfo{ ImageSource = "https://picsum.photos/50/50", Name ="Test1", ReleaseYear="1994"},
+					new MovieInfo{ ImageSource = "https://picsum.photos/50/50", Name ="Test2", ReleaseYear="1993"},
 			   };
+		  }
+
+		  private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+		  {
+			   Debug.WriteLine("selected");
+		  }
+
+		  private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+		  {
+			   Debug.WriteLine("tapped");
 		  }
 	 }
 }
